2ae39bd8906f14cf1f0adcc8d825e80286583a00
Christopher Vieira <christophervieira5112@gmail.com>
2019-11-23 12:43:37 -0500

    Started to add functionality for updating playlists in library

--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -1,4 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Library {
+    public ArrayList<Playlist> playlists = null;
+    public ArrayList<VideoSegment> videoSegments = null;
+
+    public Library(){
+
+    }
+
+    public boolean tryAddingPlaylist(String plID, String vidSegURL){
+        for (Playlist pl: playlists) {
+            if(plID.equals(pl.getID())){
+                return pl.addVideoSegment(vidSegURL);
+            }
+        }
+
+        playlists.add(new Playlist(plID, vidSegURL));
+        return true;
+    }
+
 }
--- src/main/java/model/Playlist.java
+++ src/main/java/model/Playlist.java
@@ -1,6 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Playlist {
+
+    private String id;
+    public ArrayList<String> videoSegmentURLs;
+
+    public Playlist(String id, String videoSegmentURL){
+        this.id = id;
+        videoSegmentURLs.add(videoSegmentURL);
+    }
+
+    public String getID(){
+        return id;
+    }
+
+    public boolean addVideoSegment(String videoSegURL){
+        videoSegmentURLs.add(videoSegURL);
+        return true;
+    }
 
 
 }
