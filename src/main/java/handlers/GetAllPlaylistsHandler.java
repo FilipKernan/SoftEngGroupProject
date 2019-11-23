e18dad990bda7f6ea219660e6aafb96e78fa43c9
Christopher Vieira <christophervieira5112@gmail.com>
2019-11-22 20:40:13 -0500

    Changed names of some classes, accidentally put function inside of different handler

--- src/main/java/handlers/GetAllPlaylistsHandler.java
+++ src/main/java/handlers/GetAllPlaylistsHandler.java
@@ -1,4 +1,20 @@
 package handlers;
 
-public class GetAllPlaylistsHandler {
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import http.ReturnPlaylistsResponse;
+
+public class GetAllPlaylistsHandler implements RequestHandler<Object, ReturnPlaylistsResponse> {
+
+    @Override
+    public ReturnPlaylistsResponse handleRequest(Object input, Context context){
+        return null;
+    }
 }
