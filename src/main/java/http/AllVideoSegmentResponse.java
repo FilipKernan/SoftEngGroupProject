a020bc06e6dfc05ba14aa0b62d3ef557ead1f094
FilipKernan <filip.kernan@gmail.com>
2019-11-24 11:47:36 -0500

    first pass on getLocalVideoSegments Lambda function

--- src/main/java/http/AllVideoSegmentResponse.java
+++
@@ -1,4 +0,0 @@
-package main.java.http;
-
-public class AllVideoSegmentResponce {
-}
--- src/main/java/model/VideoSegment.java
+++ src/main/java/model/VideoSegment.java
@@ -1,4 +1,16 @@
 package model;
 
 public class VideoSegment {
+
+    public final String url;
+    public final String UUID;
+    public final String transcript;
+    public final String character;
+
+    public VideoSegment(String url, String UUID, String transcript, String character) {
+        this.url = url;
+        this.UUID = UUID;
+        this.transcript = transcript;
+        this.character = character;
+    }
 }
