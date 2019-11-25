d946457bf1d3fedc76d372f2ad40eafefec9117a
emerald210 <erschmny@gmail.com>
2019-11-25 16:41:59 -0500

    Video clips now load from AWS

--- js/home.js
+++ js/home.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-    // add 5 clips programmatically
-    addVideoClip("movie.ogg");
-    addVideoClip("movie.ogg");
-    addVideoClip("movie.ogg");
-    addVideoClip("movie.ogg");
-    addVideoClip("movie.ogg");
+    getVideoSegments();
 });
 
 // adds a new video clip to the end of the slider
@@ -24,4 +19,23 @@ function addVideoClip(url) {
         "                        </div>" +
         "                        <a> </a>");
     $("#Library").append(clip);
+}
+
+// gets all video segments from AWS and creates library entries for each of them
+function getVideoSegments() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://vhrvh0my7h.execute-api.us-east-2.amazonaws.com/dev/videoSegment/get", true);
+    xhr.send();
+    console.log("sent");
+    xhr.onloadend = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            console.log("XHR:" + xhr.responseText);
+            json = JSON.parse(xhr.responseText);
+            console.log(json.list);
+            for (var i = 0; i < json.list.length; i++) {
+                url = json.list[i].url;
+                addVideoClip(url);
+            }
+        }
+    };
 }
