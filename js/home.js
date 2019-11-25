a0c0d5c7f365e885f041c55a9fb13c90fd141b4c
emerald210 <erschmny@gmail.com>
2019-11-25 17:03:31 -0500

    dynamic creation of playlist entries on home page

--- js/home.js
+++ js/home.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     getVideoSegments();
+
+    addPlaylist("https://nerdist.com/wp-content/uploads/2019/03/Star-Trek-5-Captains-star-trek-41126417-1200-630-1200x676.jpg", "test1");
+    addPlaylist("https://nerdist.com/wp-content/uploads/2019/03/Star-Trek-5-Captains-star-trek-41126417-1200-630-1200x676.jpg", "test2");
+    addPlaylist("https://nerdist.com/wp-content/uploads/2019/03/Star-Trek-5-Captains-star-trek-41126417-1200-630-1200x676.jpg", "test3");
+    addPlaylist("https://nerdist.com/wp-content/uploads/2019/03/Star-Trek-5-Captains-star-trek-41126417-1200-630-1200x676.jpg", "test4");
+    addPlaylist("https://nerdist.com/wp-content/uploads/2019/03/Star-Trek-5-Captains-star-trek-41126417-1200-630-1200x676.jpg", "test5");
 });
 
 // adds a new video clip to the end of the slider
@@ -19,6 +25,29 @@ function addVideoClip(url) {
         "                        </div>" +
         "                        <a> </a>");
     $("#Library").append(clip);
+}
+
+function addPlaylist(url, name) {
+ var playlist = $("<div class='item playlist'>\n" +
+     "                            <a>"+name+"</a>\n" +
+     "                            <br>\n" +
+     "                            <img src=\""+url+"\"\n" +
+     "                                 class=\"video\">\n" +
+     "                            <div class=\"controls\">\n" +
+     "                                <div class=\"edit_playlist\">\n" +
+     "                                    <i class=\"material-icons\">\n" +
+     "                                        edit\n" +
+     "                                    </i>\n" +
+     "                                </div>\n" +
+     "                                <div class=\"delete_playlist\">\n" +
+     "                                    <i class=\"material-icons\">\n" +
+     "                                        close\n" +
+     "                                    </i>\n" +
+     "                                </div>\n" +
+     "                            </div>\n" +
+     "                        </div>" +
+     "                        <a> </a>");
+ $("#Playlist").append(playlist);
 }
 
 // gets all video segments from AWS and creates library entries for each of them
