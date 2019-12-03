13cc53713abdb595d39b83c9fea4e73d3752efd5
Christopher Vieira <christophervieira5112@gmail.com>
2019-12-03 17:54:24 -0500

    Worked on the delete video segment handler, currently contains no details on bucket/keys. Discuss this at next meeting tomorrow

--- src/main/java/handlers/DeleteVideoSegmentHandler.java
+++ src/main/java/handlers/DeleteVideoSegmentHandler.java
@@ -1,8 +1,11 @@
 package handlers;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import db.PlaylistDAO;
 import db.VideoSegmentDAO;
 import http.DeletePlaylistRequest;
@@ -14,6 +17,8 @@ public class DeleteVideoSegmentHandler implements RequestHandler<DeleteVideoSegm
 
     public LambdaLogger logger;
 
+    private AmazonS3 s3 = null;
+
     boolean deleteVideoSegment(String id) throws Exception {
         VideoSegmentDAO dao = new VideoSegmentDAO();
 
@@ -22,10 +27,24 @@ public class DeleteVideoSegmentHandler implements RequestHandler<DeleteVideoSegm
 
     @Override
     public DeleteVideoSegmentResponse handleRequest(DeleteVideoSegmentRequest input, Context context){
+        logger = context.getLogger();
+        logger.log(input.toString());
         DeleteVideoSegmentResponse response;
         String id = input.getID();
         try{
+
+            if (logger != null) { logger.log("in createSystemConstant"); }
+
+            if (s3 == null) {
+                logger.log("attach to S3 request");
+                s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+                logger.log("attach to S3 succeed");
+            }
+            //TODO
+            s3.deleteObject("NAME OF THE BUCKET", "NAME OF THE KEY");
+
             boolean success = deleteVideoSegment(id);
+
             if(success){
                 response = new DeleteVideoSegmentResponse(id, 200);
             } else {
