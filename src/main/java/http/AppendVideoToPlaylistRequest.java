d57936552ee010a2947f08d5d890e54894be596b
Christopher Vieira <christophervieira5112@gmail.com>
2019-12-04 13:14:29 -0500

    Fixed http request for append video to playlist

--- src/main/java/http/AppendVideoToPlaylistRequest.java
+++ src/main/java/http/AppendVideoToPlaylistRequest.java
@@ -4,9 +4,21 @@ public class AppendVideoToPlaylistRequest {
     private String playlistID;
     private String videoID;
 
+    public void setPlaylistID(String playlistID) {
+        this.playlistID = playlistID;
+    }
+
+    public void setVideoID(String videoID) {
+        this.videoID = videoID;
+    }
+
     public AppendVideoToPlaylistRequest(String playlistID, String videoID){
         this.playlistID = playlistID;
         this.videoID = videoID;
+    }
+
+    public AppendVideoToPlaylistRequest(){
+
     }
 
     public String getPlaylistID() {
