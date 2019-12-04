a602ecff5d803e5ed71f8b4c9cc4cd363ec42327
Christopher Vieira <christophervieira5112@gmail.com>
2019-12-04 13:12:51 -0500

    Changed naming of http request/response for get all playlists functionality

--- src/main/java/handlers/GetAllPlaylistsHandler.java
+++ src/main/java/handlers/GetAllPlaylistsHandler.java
@@ -1,23 +1,13 @@
 package handlers;
 
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.*;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import db.PlaylistDAO;
-import http.ReturnPlaylistsResponse;
+import http.GetAllPlaylistsResponse;
 import model.Playlist;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class GetAllPlaylistsHandler implements RequestHandler<Object, ReturnPlaylistsResponse> {
+public class GetAllPlaylistsHandler implements RequestHandler<Object, GetAllPlaylistsResponse> {
 
     public LambdaLogger logger;
 
@@ -28,14 +18,14 @@ public class GetAllPlaylistsHandler implements RequestHandler<Object, ReturnPlay
     }
 
     @Override
-    public ReturnPlaylistsResponse handleRequest(Object input, Context context){
-       ReturnPlaylistsResponse response;
+    public GetAllPlaylistsResponse handleRequest(Object input, Context context){
+       GetAllPlaylistsResponse response;
         try{
             List<Playlist> list = getPlaylists();
-            response = new ReturnPlaylistsResponse(list, 200);
+            response = new GetAllPlaylistsResponse(list, 200);
 
         } catch (Exception e) {
-            response = new ReturnPlaylistsResponse(403, e.getMessage());
+            response = new GetAllPlaylistsResponse(403, e.getMessage());
         }
         return response;
     }
