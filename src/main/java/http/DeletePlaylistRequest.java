90ecb41fa59d5e555e761d70fc8f1a4aa02227cc
Christopher Vieira <christophervieira5112@gmail.com>
2019-12-04 11:27:13 -0500

    Request was done improperly

--- src/main/java/http/DeletePlaylistRequest.java
+++ src/main/java/http/DeletePlaylistRequest.java
@@ -1,10 +1,18 @@
 package http;
 
 public class DeletePlaylistRequest {
+    public void setId(String id) {
+        this.id = id;
+    }
+
     private String id;
 
     public DeletePlaylistRequest(String id){
         this.id = id;
+    }
+
+    public DeletePlaylistRequest(){
+
     }
 
 
